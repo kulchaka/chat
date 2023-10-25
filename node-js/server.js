8ffb89570371b88bf528cf045f4ca306067a7c01
Andrii <andrii.kulchytskyi@xorux.com>
2023-10-25 11:22:18 +0200

    add express.js, socket.io & ANTD

--- node-js/server.js
+++ node-js/server.js
@@ -1,11 +1,21 @@
 const express = require("express")
-const app = express()
+const { createServer } = require("node:http")
+const { Server } = require("socket.io")
+
 const port = 9999
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
 
 app.get("/", (req, res) => {
   res.send("Hello CHAT!")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id)
+})
+
+server.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`)
 })
--- ui-react/src/App.tsx
+++ ui-react/src/App.tsx
@@ -1,9 +1,16 @@
+import { Button } from "antd"
+import { io } from "socket.io-client"
 
 function App() {
+
+  const connectSocket = () => {
+    io('http://localhost:9999/')
+  }
 
   return (
     <>
       CHAT
+      <Button onClick={connectSocket}>TEST</Button>
     </>
   )
 }
