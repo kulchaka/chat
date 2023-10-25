dd514d038d877e9ae1fd84935af28cd3e6cff4d6
Andrii <andrii.kulchytskyi@xorux.com>
2023-10-25 11:51:17 +0200

    ui: add Form Enter

--- ui-react/src/App.tsx
+++ ui-react/src/App.tsx
@@ -1,17 +1,57 @@
-import { Button } from "antd"
+import { Button, ConfigProvider, Form, Input } from "antd"
 import { io } from "socket.io-client"
 
 function App() {
 
-  const connectSocket = () => {
+  const [form] = Form.useForm()
+
+  const testConnect = () => {
     io('http://localhost:9999/')
+    form.resetFields()
+  }
+
+  const onFinish = (values: unknown) => {
+    console.log("💡 ~ onFinish ~ values:", values)
   }
 
   return (
-    <>
-      CHAT
-      <Button onClick={connectSocket}>TEST</Button>
-    </>
+    <div className="block">
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#272727",
+          },
+          components: {
+            Button: {
+              colorPrimary: "#000",
+
+            },
+          },
+        }}
+      >
+        <Form
+          form={form}
+          name="control-hooks"
+          onFinish={onFinish}
+          style={{ maxWidth: 600 }}
+        >
+          <Form.Item name="roomID" rules={[{ required: true, message: 'Please input Room ID!' }]}>
+            <Input placeholder="Room ID" allowClear />
+          </Form.Item>
+          <Form.Item name="name" rules={[{ required: true, message: 'Please input your Name!' }]}>
+            <Input placeholder="Name" allowClear />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Sign in
+            </Button>
+          </Form.Item>
+          <Button htmlType="button" onClick={testConnect}>
+            TEST
+          </Button>
+        </Form>
+      </ConfigProvider>
+    </div>
   )
 }
 
