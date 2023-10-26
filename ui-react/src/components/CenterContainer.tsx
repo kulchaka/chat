868501ee696bec5e6e47ab418c7aa6de359c1c34
Andrii <andrii.kulchytskyi@xorux.com>
2023-10-26 13:51:32 +0200

    add POST method API & upd ui

--- ui-react/src/components/CenterContainer.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, ReactNode } from "react"
-
-interface Props {
-  children: ReactNode
-}
-
-const CenterContainer: FC<Props> = ({ children }) => <div className="block">{children}</div>
-
-export default CenterContainer
